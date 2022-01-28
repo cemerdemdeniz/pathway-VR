b5b43ce6d24ca95af6e892b04b83bb9964b4a7ad
cemerdemdeniz <c-e-d@outlook.com.tr>
2022-01-28 06:16:59 +0000

    Added dijkstra , projectile

---
+++ Assets/Scripts/ProjectTile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectTile : MonoBehaviour
+{
+    public GameObject bullet;
+
+    public float shootForce;   
+
+    public Camera cam;
+
+    public Transform attackPoint;
+   
+   
+    public void Fire()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else 
+           targetPoint = ray.GetPoint(75);
+
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+
+        currentBullet.transform.forward = directionWithoutSpread.normalized;
+
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithoutSpread.normalized * shootForce, ForceMode.Impulse);
+      
+       
+    }
+
+   
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject gun;
+  public void ChangeColourGreen()
+    {
+       gun.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void ChangeColourPurple()
+    {
+        gun.GetComponent<Renderer>().material.color = Color.magenta;
+    }
+
+    public void ChangeColourBlue()
+    {
+        gun.GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
