fc063f9a9c9141e42e50eebb96514b19c142fc91
cemerdemdeniz <c-e-d@outlook.com.tr>
2022-01-31 15:51:51 +0000

    Main Menu UI fix

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
@@ -9,33 +10,63 @@ public class MainMenuUI : MonoBehaviour
     public GameObject _difficulty;
     public void LoadNextScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);      
-        
+        StartCoroutine(LoadNextScene(1));
     }
 
     public void OptionsMenu()
     {
-        _start.SetActive(false);
-        _optionMenu.SetActive(false);
-        _backToMenu.SetActive(true);
-        _difficulty.SetActive(true);
+        StartCoroutine(OptionsMenu(1));
     }
 
     public void BackToMenu()
     {
-        _start.SetActive(true);
-        _optionMenu.SetActive(true);
-        _backToMenu.SetActive(false);
-        _difficulty.SetActive(false);
+        StartCoroutine(BackToMenu(1));
     }
 
     public void Difficulty()
     {
+        StartCoroutine(Difficulty(1));
+    }
+
+
+    #region Numerators
+    IEnumerator OptionsMenu(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _start.SetActive(false);
+        _optionMenu.SetActive(false);
+        _backToMenu.SetActive(true);
+        _difficulty.SetActive(true);
+
+    }
+    IEnumerator LoadNextScene(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    IEnumerator BackToMenu(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _start.SetActive(true);
+        _optionMenu.SetActive(true);
+        _backToMenu.SetActive(false);
+        _difficulty.SetActive(false);
+
+
+    }
+
+    IEnumerator Difficulty(float delay)
+    {
+        yield return new WaitForSeconds(delay);
         _start.SetActive(false);
         _optionMenu.SetActive(false);
         _backToMenu.SetActive(false);
         _difficulty.SetActive(false);
+
+
     }
+    #endregion
 
 
 
